120c713013beb1a9e26f6ee68b96f6608fc10fce
Ze <88213402+Z-Barriga@users.noreply.github.com>
2023-01-13 15:05:26 +0000

    Update Js.js

--- Js.js
+++ Js.js
@@ -1,12 +1,16 @@
-// Select all the sections 
+// Select all buttons
+const buttons = document.querySelectorAll('button');
+// Select all sections
 const sections = document.querySelectorAll('.section');
 
-// Add click event listener to all the sections
-sections.forEach(section => {
-    section.addEventListener('click', e => {
-        // Remove the active class from all sections
-        sections.forEach(section => section.classList.remove('active'));
-        // Add the active class to the clicked section
-        e.target.classList.add('active');
+// Add event listener to all buttons
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        // Remove active class from all sections
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+        // Add active class to the corresponding section based on the button clicked
+        document.getElementById(e.target.id.replace('-button','')).classList.add('active');
     });
 });
