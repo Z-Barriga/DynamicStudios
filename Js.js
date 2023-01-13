4defe26d9a7c96c32ff1089594585946b81c7db3
Ze <88213402+Z-Barriga@users.noreply.github.com>
2023-01-13 15:30:57 +0000

    Update Js.js

--- Js.js
+++ Js.js
@@ -14,3 +14,12 @@ buttons.forEach(button => {
         document.getElementById(e.target.id.replace('-button','')).classList.add('active');
     });
 });
+// Add click event listener to all the sections
+sections.forEach(section => {
+    section.addEventListener('click', e => {
+        // Remove the active class from all sections
+        sections.forEach(section => section.classList.remove('active'));
+        // Add the active class to the clicked section
+        e.target.classList.add('active');
+    });
+});
